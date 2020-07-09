a034ea65b881c7bb830c71f6a413d855257342c2
MiranDaniel <mirandanielcz@gmail.com>
2020-07-09 16:05:45 +0200

    Update download.py

--- download.py
+++ download.py
@@ -1,16 +1,54 @@
-import json
+import requests
 import urllib.request
+import json
+import ffmpeg
 
 
+SAVE_PATH = r""
 
-def download(submission_id, path):
-    uf = urllib.request.urlopen(f"https://www.reddit.com/{submission_id}.json")
+def get(submission):
+    uf = urllib.request.urlopen(f"https://www.reddit.com/{submission}.json")
     html = uf.read()
     y = json.loads(html)
     url = str(y[0]["data"]["children"][0]["data"]["media"]["reddit_video"]["fallback_url"]).replace("?source=fallback","")
-    x = url[-6:].find(".")
-    file_format = url[-6:][x:]
-    name = f'{submission_id}{file_format}'
-    urllib.request.urlretrieve(url, path+name)
+    video_url = url
+    if ".mp4" in url:
+        start = url.find("_")
+        end = url[-6:].find(".")+len(url)
 
+        if end == -1 or 0:
+            audio_url = url.replace(url[start:],"_audio.mp4")
+        else:
+            audio_url = url.replace(url[start:end],"_audio.mp4")
+
+    else:
+        start = url[-10:].find("/")
+        audio_url = url.replace(url[start+len(url)-10:],"/audio")
+        
+
+    return video_url, audio_url
+    
+
+
+def download(submission):
+    urls = get(submission)
+    video_url = urls[0]
+    audio_url = urls[1]
+    
+    video_name = f"/video_{submission}.mp4"
+    audio_name = f"/audio_{submission}.mp4"
+
+    urllib.request.urlretrieve(video_url, SAVE_PATH+video_name)
+    urllib.request.urlretrieve(audio_url, SAVE_PATH+audio_name)
+
+
+    merge(submission)
+def merge(submission):
+    video_name = f"/video_{submission}.mp4"
+    audio_name = f"/audio_{submission}.mp4"
+
+    input_video = ffmpeg.input(SAVE_PATH+video_name)
+    input_audio = ffmpeg.input(SAVE_PATH+audio_name)
+
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(f"{SAVE_PATH}/final_{submission}.mp4").run()
 
